06484ca06ea4126faeda6dd9f49ed0e8c2886f04
DG <donavingrouette@live.com>
2019-06-09 12:40:58 -0500

    Final: Alpha - Mostly Tested

--- SD240_CalcProject/MainWindow.xaml.cs
+++ SD240_CalcProject/MainWindow.xaml.cs
@@ -382,7 +382,8 @@ namespace SD240_CalcProject
             else
             {
                 ConversionBox.Visibility = Visibility.Visible;
-                ProgrammerConversions("4", "101203", "123123", "7");
+                ProgrammerConversions(InputBox.Text);
+                ConvertActiveInput();
                 Application.Current.MainWindow.Width = 820;
             }
         }
@@ -390,14 +391,253 @@ namespace SD240_CalcProject
 
         public void SetActiveOutput(object sender, RoutedEventArgs e)
         {
-            var x = (TextBox)e.Source;
-            Enum.TryParse(x.Name, out CurrentActiveOutput);
+            var textBox = (TextBox)e.Source;
+            Enum.TryParse(textBox.Name, out CurrentActiveOutput);
         }
 
 
         private void Button_Click_IntiateConversions(object sender, RoutedEventArgs e)
         {
             // Determine which conversion we're on,
+            ConvertActiveInput();
+        }
+
+        private void Button_Click_ProgrammerConversions(object sender, RoutedEventArgs e)
+        {
+            ProgrammerConversions(InputBox.Text);
+            ConvertActiveInput();
+        }
+
+        private void Button_Click_PercentConversions(object sender, RoutedEventArgs e)
+        {
+            PercentConversions(InputBox.Text);
+            ConvertActiveInput();
+        }
+
+        private void Button_Click_WeightConversions(object sender, RoutedEventArgs e)
+        {
+            WeightConversions(InputBox.Text);
+            ConvertActiveInput();
+        }
+
+        private void Button_Click_TemperatureConversions(object sender, RoutedEventArgs e)
+        {
+            TemperatureConversions(InputBox.Text);
+            ConvertActiveInput();
+        }
+
+        private void Button_Click_LengthConversions(object sender, RoutedEventArgs e)
+        {
+            LengthConversions(InputBox.Text);
+            ConvertActiveInput();
+        }
+
+        private void Button_Click_FileSizeConversions(object sender, RoutedEventArgs e)
+        {
+            DataConversions(InputBox.Text);
+            ConvertActiveInput();
+        }
+
+        private void Button_Click_TimeConversions(object sender, RoutedEventArgs e)
+        {
+            TimeConversions(InputBox.Text);
+            ConvertActiveInput();
+        }
+
+
+        private void ProgrammerConversions(string val1, string val2 = "", string val3 = "", string val4 = "")
+        {
+            CurrentConversionType = ConversionTypes.Programmer;
+            HideClearAllGroups();
+            CurrentActiveOutput = ActiveOutput.TextOutput1;
+            SetGroup(Groups.Group1, "Integer", val1);
+            SetGroup(Groups.Group2, "Hexadecimal", val2);
+            SetGroup(Groups.Group3, "Octal", val3);
+            SetGroup(Groups.Group4, "Binary", val4);
+        }
+
+        private void PercentConversions(string val1, string val2 = "")
+        {
+            CurrentConversionType = ConversionTypes.Percent;
+            HideClearAllGroups();
+            CurrentActiveOutput = ActiveOutput.TextOutput1;
+            SetGroup(Groups.Group1, "Decimal", val1);
+            SetGroup(Groups.Group2, "Percent", val2);
+        }
+
+        private void WeightConversions(string val1, string val2 = "", string val3 = "", string val4 = "", string val5 = "")
+        {
+            CurrentConversionType = ConversionTypes.Weight;
+            HideClearAllGroups();
+            CurrentActiveOutput = ActiveOutput.TextOutput1;
+            SetGroup(Groups.Group1, "MilliGrams", val1);
+            SetGroup(Groups.Group2, "Grams", val2);
+            SetGroup(Groups.Group3, "KiloGrams", val3);
+            SetGroup(Groups.Group4, "Ounces", val4);
+            SetGroup(Groups.Group5, "Pounds", val5);
+        }
+
+        private void TemperatureConversions(string val1, string val2 = "", string val3 = "")
+        {
+            CurrentConversionType = ConversionTypes.Temperature;
+            HideClearAllGroups();
+            CurrentActiveOutput = ActiveOutput.TextOutput1;
+            SetGroup(Groups.Group1, "Celcius", val1);
+            SetGroup(Groups.Group2, "Fahrenheit", val2);
+            SetGroup(Groups.Group3, "Kelvin", val3);
+        }
+
+        private void LengthConversions(string val1, string val2 = "", string val3 = "", string val4 = "", string val5 = "", string val6 = "")
+        {
+            CurrentConversionType = ConversionTypes.Length;
+            HideClearAllGroups();
+            CurrentActiveOutput = ActiveOutput.TextOutput1;
+            SetGroup(Groups.Group1, "MilliMeters", val1);
+            SetGroup(Groups.Group2, "CentiMeters", val2);
+            SetGroup(Groups.Group3, "Meters", val3);
+            SetGroup(Groups.Group4, "KiloMeters", val4);
+            SetGroup(Groups.Group5, "Inches", val5);
+            SetGroup(Groups.Group6, "Feet", val6);
+        }
+
+        private void DataConversions(string val1, string val2 = "", string val3 = "", string val4 = "", string val5 = "")
+        {
+            CurrentConversionType = ConversionTypes.Data;
+            HideClearAllGroups();
+            CurrentActiveOutput = ActiveOutput.TextOutput1;
+            SetGroup(Groups.Group1, "Bytes", val1);
+            SetGroup(Groups.Group2, "KiloBytes", val2);
+            SetGroup(Groups.Group3, "MegaBytes", val3);
+            SetGroup(Groups.Group4, "GigaBytes", val4);
+            SetGroup(Groups.Group5, "TeraBytes", val5);
+        }
+
+        private void TimeConversions(string val1, string val2 = "", string val3 = "")
+        {
+            CurrentConversionType = ConversionTypes.Time;
+            HideClearAllGroups();
+            CurrentActiveOutput = ActiveOutput.TextOutput1;
+            SetGroup(Groups.Group1, "Seconds", val1);
+            SetGroup(Groups.Group2, "Minutes", val2);
+            SetGroup(Groups.Group3, "Hours", val3);
+        }
+
+        private void HideClearAllGroups()
+        {
+            TextLabel1.Text = null;
+            TextOutput1.Text = null;
+
+            TextLabel2.Text = null;
+            TextOutput2.Text = null;
+
+            TextLabel3.Text = null;
+            TextOutput3.Text = null;
+
+            TextLabel4.Text = null;
+            TextOutput4.Text = null;
+
+            TextLabel5.Text = null;
+            TextOutput5.Text = null;
+
+            TextLabel6.Text = null;
+            TextOutput6.Text = null;
+
+            Group1.Visibility = Visibility.Hidden;
+            Group2.Visibility = Visibility.Hidden;
+            Group3.Visibility = Visibility.Hidden;
+            Group4.Visibility = Visibility.Hidden;
+            Group5.Visibility = Visibility.Hidden;
+            Group6.Visibility = Visibility.Hidden;
+        }
+
+        private void SetGroup(Groups group, string label, string val)
+        {
+            switch (group)
+            {
+                case Groups.Group1:
+                    TextLabel1.Text = label;
+                    TextOutput1.Text = val;
+                    if (Group1.Visibility == Visibility.Hidden)
+                        Group1.Visibility = Visibility.Visible;
+                    break;
+
+                case Groups.Group2:
+                    TextLabel2.Text = label;
+                    TextOutput2.Text = val;
+                    if (Group2.Visibility == Visibility.Hidden)
+                        Group2.Visibility = Visibility.Visible;
+                    break;
+
+                case Groups.Group3:
+                    TextLabel3.Text = label;
+                    TextOutput3.Text = val;
+                    if (Group3.Visibility == Visibility.Hidden)
+                        Group3.Visibility = Visibility.Visible;
+                    break;
+
+                case Groups.Group4:
+                    TextLabel4.Text = label;
+                    TextOutput4.Text = val;
+                    if (Group4.Visibility == Visibility.Hidden)
+                        Group4.Visibility = Visibility.Visible;
+                    break;
+
+                case Groups.Group5:
+                    TextLabel5.Text = label;
+                    TextOutput5.Text = val;
+                    if (Group5.Visibility == Visibility.Hidden)
+                        Group5.Visibility = Visibility.Visible;
+                    break;
+
+                case Groups.Group6:
+                    TextLabel6.Text = label;
+                    TextOutput6.Text = val;
+                    if (Group6.Visibility == Visibility.Hidden)
+                        Group6.Visibility = Visibility.Visible;
+                    break;
+
+                default:
+                    new Exception("Error - out of place enum val");
+                    break;
+            }
+        }
+
+        private ConversionTypes CurrentConversionType = ConversionTypes.Programmer;
+        private ActiveOutput CurrentActiveOutput = ActiveOutput.TextOutput1;
+
+        private enum Groups
+        {
+            Group1,
+            Group2,
+            Group3,
+            Group4,
+            Group5,
+            Group6
+        }
+
+        private enum ConversionTypes
+        {
+            Programmer,
+            Percent,
+            Weight,
+            Temperature,
+            Length,
+            Data,
+            Time
+        }
+
+        private enum ActiveOutput
+        {
+            TextOutput1,
+            TextOutput2,
+            TextOutput3,
+            TextOutput4,
+            TextOutput5,
+            TextOutput6
+        }
+
+        private void ConvertActiveInput()
+        {
             switch (CurrentConversionType)
             {
                 // == PROGRAMER ==
@@ -1145,224 +1385,6 @@ namespace SD240_CalcProject
                     TextOutput6.Text = "NaN";
                     break;
             }
-        }
-
-        private void Button_Click_ProgrammerConversions(object sender, RoutedEventArgs e)
-        {
-            ProgrammerConversions(InputBox.Text, String.Empty, String.Empty, String.Empty);
-        }
-
-        private void Button_Click_PercentConversions(object sender, RoutedEventArgs e)
-        {
-            PercentConversions(InputBox.Text, String.Empty);
-        }
-
-        private void Button_Click_WeightConversions(object sender, RoutedEventArgs e)
-        {
-            WeightConversions("", "", "", "", "");
-        }
-
-        private void Button_Click_TemperatureConversions(object sender, RoutedEventArgs e)
-        {
-            TemperatureConversions("", "", "");
-        }
-
-        private void Button_Click_LengthConversions(object sender, RoutedEventArgs e)
-        {
-            LengthConversions("", "", "", "", "", "");
-        }
-
-        private void Button_Click_FileSizeConversions(object sender, RoutedEventArgs e)
-        {
-            DataConversions("", "", "", "", "");
-        }
-
-        private void Button_Click_TimeConversions(object sender, RoutedEventArgs e)
-        {
-            TimeConversions("", "", "");
-        }
-
-        private void ProgrammerConversions(string val1, string val2, string val3, string val4)
-        {
-            CurrentConversionType = ConversionTypes.Programmer;
-            HideClearAllGroups();
-            SetGroup(Groups.Group1, "Integer", val1);
-            SetGroup(Groups.Group2, "Hexadecimal", val2);
-            SetGroup(Groups.Group3, "Octal", val3);
-            SetGroup(Groups.Group4, "Binary", val4);
-        }
-
-        private void PercentConversions(string val1, string val2)
-        {
-            CurrentConversionType = ConversionTypes.Percent;
-            HideClearAllGroups();
-            SetGroup(Groups.Group1, "Decimal", val1);
-            SetGroup(Groups.Group2, "Percent", val2);
-        }
-
-        private void WeightConversions(string val1, string val2, string val3, string val4, string val5)
-        {
-            CurrentConversionType = ConversionTypes.Weight;
-            HideClearAllGroups();
-            SetGroup(Groups.Group1, "MilliGrams", val1);
-            SetGroup(Groups.Group2, "Grams", val2);
-            SetGroup(Groups.Group3, "KiloGrams", val3);
-            SetGroup(Groups.Group4, "Ounces", val4);
-            SetGroup(Groups.Group5, "Pounds", val5);
-        }
-
-        private void TemperatureConversions(string val1, string val2, string val3)
-        {
-            CurrentConversionType = ConversionTypes.Temperature;
-            HideClearAllGroups();
-            SetGroup(Groups.Group1, "Celcius", val1);
-            SetGroup(Groups.Group2, "Fahrenheit", val2);
-            SetGroup(Groups.Group3, "Kelvin", val3);
-        }
-
-        private void LengthConversions(string val1, string val2, string val3, string val4, string val5, string val6)
-        {
-            CurrentConversionType = ConversionTypes.Length;
-            HideClearAllGroups();
-            SetGroup(Groups.Group1, "MilliMeters", val1);
-            SetGroup(Groups.Group2, "CentiMeters", val2);
-            SetGroup(Groups.Group3, "Meters", val3);
-            SetGroup(Groups.Group4, "KiloMeters", val4);
-            SetGroup(Groups.Group5, "Inches", val5);
-            SetGroup(Groups.Group6, "Feet", val6);
-        }
-        private void DataConversions(string val1, string val2, string val3, string val4, string val5)
-        {
-            CurrentConversionType = ConversionTypes.Data;
-            HideClearAllGroups();
-            SetGroup(Groups.Group1, "Bytes", val1);
-            SetGroup(Groups.Group2, "KiloBytes", val2);
-            SetGroup(Groups.Group3, "MegaBytes", val3);
-            SetGroup(Groups.Group4, "GigaBytes", val4);
-            SetGroup(Groups.Group5, "TeraBytes", val5);
-        }
-
-        private void TimeConversions(string val1, string val2, string val3)
-        {
-            CurrentConversionType = ConversionTypes.Time;
-            HideClearAllGroups();
-            SetGroup(Groups.Group1, "Seconds", val1);
-            SetGroup(Groups.Group2, "Minutes", val2);
-            SetGroup(Groups.Group3, "Hours", val3);
-        }
-
-        private void HideClearAllGroups()
-        {
-            TextLabel1.Text = null;
-            TextOutput1.Text = null;
-
-            TextLabel2.Text = null;
-            TextOutput2.Text = null;
-
-            TextLabel3.Text = null;
-            TextOutput3.Text = null;
-
-            TextLabel4.Text = null;
-            TextOutput4.Text = null;
-
-            TextLabel5.Text = null;
-            TextOutput5.Text = null;
-
-            TextLabel6.Text = null;
-            TextOutput6.Text = null;
-
-            Group1.Visibility = Visibility.Hidden;
-            Group2.Visibility = Visibility.Hidden;
-            Group3.Visibility = Visibility.Hidden;
-            Group4.Visibility = Visibility.Hidden;
-            Group5.Visibility = Visibility.Hidden;
-            Group6.Visibility = Visibility.Hidden;
-        }
-
-        private void SetGroup(Groups group, string label, string val)
-        {
-            switch (group)
-            {
-                case Groups.Group1:
-                    TextLabel1.Text = label;
-                    TextOutput1.Text = val;
-                    if (Group1.Visibility == Visibility.Hidden)
-                        Group1.Visibility = Visibility.Visible;
-                    break;
-
-                case Groups.Group2:
-                    TextLabel2.Text = label;
-                    TextOutput2.Text = val;
-                    if (Group2.Visibility == Visibility.Hidden)
-                        Group2.Visibility = Visibility.Visible;
-                    break;
-
-                case Groups.Group3:
-                    TextLabel3.Text = label;
-                    TextOutput3.Text = val;
-                    if (Group3.Visibility == Visibility.Hidden)
-                        Group3.Visibility = Visibility.Visible;
-                    break;
-
-                case Groups.Group4:
-                    TextLabel4.Text = label;
-                    TextOutput4.Text = val;
-                    if (Group4.Visibility == Visibility.Hidden)
-                        Group4.Visibility = Visibility.Visible;
-                    break;
-
-                case Groups.Group5:
-                    TextLabel5.Text = label;
-                    TextOutput5.Text = val;
-                    if (Group5.Visibility == Visibility.Hidden)
-                        Group5.Visibility = Visibility.Visible;
-                    break;
-
-                case Groups.Group6:
-                    TextLabel6.Text = label;
-                    TextOutput6.Text = val;
-                    if (Group6.Visibility == Visibility.Hidden)
-                        Group6.Visibility = Visibility.Visible;
-                    break;
-
-                default:
-                    new Exception("Error - out of place enum val");
-                    break;
-            }
-        }
-
-        private ConversionTypes CurrentConversionType = ConversionTypes.Programmer;
-        private ActiveOutput CurrentActiveOutput = ActiveOutput.TextOutput1;
-
-        private enum Groups
-        {
-            Group1,
-            Group2,
-            Group3,
-            Group4,
-            Group5,
-            Group6
-        }
-
-        private enum ConversionTypes
-        {
-            Programmer,
-            Percent,
-            Weight,
-            Temperature,
-            Length,
-            Data,
-            Time
-        }
-
-        private enum ActiveOutput
-        {
-            TextOutput1,
-            TextOutput2,
-            TextOutput3,
-            TextOutput4,
-            TextOutput5,
-            TextOutput6
         }
 
     }
